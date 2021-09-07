79c5bcedc5e1c9938f4e0ae9dc8f5029c7cd9722
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-09-07 15:33:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,60 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const blogArr = [
+    {
+      title: 'Title1',
+      description: 'This is just random text'
+    },
+    {
+      title: 'Title2',
+      description: 'This is just random text'
+    },
+    {
+      title: 'Title3',
+      description: 'This is just random text'
+    }
+  ]
+
+  const obj1 = {
+    title: 'Object1',
+    description: 'This is just random text object'
+  }
+
+  const obj2 = {
+    title: 'Object2',
+    description: 'This is just random text object'
+  }
+
+  const blogElements =  {
+    // margin:16px,
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style = {
+      {
+        margin: '16px',
+        Padding: '10px',
+        borderRadius: '5px',
+        boxShadow: '0 2px 5px #ccc',
+        background: 'Black',
+        color: 'white'
+      }
+    }>
+      <h1>Array elements: {blogArr[0].title}</h1>
+      <h3>This is a test</h3>
+      <h2>Object elements: {obj1.title}</h2>
+      <hr />
+      <h1>Array elements: {blogArr[0].title}</h1>
+      <h3>This is a test</h3>
+      <h2>Object elements: {obj1.title}</h2>
+      <hr />
+      <h1>Array elements: {blogArr[0].title}</h1>
+      <h3>This is a test</h3>
+      <h2>Object elements: {obj1.title}</h2>
+      <hr />
     </div>
   );
 }
